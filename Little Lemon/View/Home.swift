8c3d7d6a88a3c884e6a1ab75eff473af11837397
Rohan Dave <rohandewangdave@gmail.com>
2024-09-26 18:35:27 -0400

    completed code

---
+++ Little Lemon/View/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  Little Lemon
+//
+//  Created by Rohan Dave on 26/09/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
