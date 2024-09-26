35f45fc405db611e42e9c542ff2dc2aabbbcdf87
Rohan Dave <rohandewangdave@gmail.com>
2024-09-26 16:06:29 -0400

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Rohan Dave on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Little LemonTests/Little_LemonTests.swift
@@ -0,0 +1,16 @@
+//
+//  Little_LemonTests.swift
+//  Little LemonTests
+//
+//  Created by Rohan Dave on 26/09/24.
+//
+
+import Testing
+
+struct Little_LemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
